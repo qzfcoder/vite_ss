92c3816fa04c4bc46ac0cb90995d84193d760a62
qzf <867303253@qq.com>
2025-04-15 10:21:32 +0800

    fix()v3版本依赖安装

--- vite-v3/src/App.tsx
+++ vite-v3/src/App.tsx
@@ -1,15 +1,21 @@
 // HelloWorld.tsx
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
+// import "./styles/index.css";
+import classes from "@/styles/test.module.css";
+import "@/styles/test.less";
+import { a } from "./test";
+import { A } from "./type";
 const HelloWorld = defineComponent({
-    name: 'HelloWorld',
-    setup() {
-        return () => (
-            <div>
-                <h1>Hello, Vue + TypeScript + JSX!</h1>
-            </div>
-        );
-    },
+  name: "HelloWorld",
+  setup() {
+    return () => (
+      //   <div class={`root ${classes.moduleClass}`}>
+      <div class={`root`}>
+        <h1>Hello, Vue + TypeScript + JSX!{a.name}</h1>
+      </div>
+    );
+  },
 });
 
 export default HelloWorld;
---
+++ vite-v3/src/test.ts
@@ -0,0 +1,9 @@
+import { A } from './type'
+import.meta.url
+
+export const a: A = {
+  name: '小丑',
+  // age: 18
+}
+
+// export { A }
